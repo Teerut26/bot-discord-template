548762af0262c679b51d796abb06bd884542f712
Teerut26 <teerutsr@gmail.com>
2022-02-28 23:51:03 +0700

    add command option and add enum option.

--- src/register-command.ts
+++ src/register-command.ts
@@ -1,3 +1,5 @@
+import { optionsType } from "./enum/command_option_type";
+
 require("dotenv").config();
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
@@ -7,32 +9,36 @@ export default class RegisterCommand {
     {
       name: "ping",
       description: "Replies with Pong!",
+      options: [
+        { name: "number", description: "number", required: false, type: optionsType.INTEGER },
+      ],
     },
   ];
 
+  // ref https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
   private client_id = process.env.CLIENT_ID;
   private guild_id = process.env.GUILD_ID;
   private rest: any;
 
   constructor() {
     this.rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
-    this.register()
+    this.register();
   }
 
-  register() : void {
+  register(): void {
     (async () => {
-        try {
-          console.log("Started refreshing application (/) commands.");
-  
-          await this.rest.put(
-            Routes.applicationGuildCommands(this.client_id, this.guild_id),
-            { body: this.commands }
-          );
-  
-          console.log("Successfully reloaded application (/) commands.");
-        } catch (error) {
-          console.error(error);
-        }
-      })();
+      try {
+        console.log("Started refreshing application (/) commands.");
+
+        await this.rest.put(
+          Routes.applicationGuildCommands(this.client_id, this.guild_id),
+          { body: this.commands }
+        );
+
+        console.log("Successfully reloaded application (/) commands.");
+      } catch (error) {
+        console.error(error);
+      }
+    })();
   }
 }
